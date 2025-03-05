c9b91aac5e0c5ba911936fc625cffcc04eaf69b9
Judy <158211720+judyyan27@users.noreply.github.com>
2025-03-05 11:54:25 -0500

    save

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
-import { RouterView } from 'vue-router'
+//import { RouterView } from 'vue-router'
 import ApiDisplay from './components/ApiDisplay.vue'
 </script>
 
 <template>
-  <RouterView />
   <ApiDisplay />
 </template>
 
--- app/src/components/ApiDisplay.vue
+++ app/src/components/ApiDisplay.vue
@@ -1,23 +1,23 @@
 <template>
-  <h2>{{ pokemon.name }}</h2>
+  <h2>{{ data }}</h2>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
-const MVC = ref('')
+const data = ref('')
 
-async function getPokemon() {
+async function getData() {
   let res = await fetch('https://data.cityofnewyork.us/resource/bm4k-52h4.json')
-  let data = await res.json
-  MVC.value = data.results
+  let data = await res.json()
+  data.value = data.results
   console.log(data)
 }
 
-/* onMounted(() => {
-  getPokemon()
-}) */
+onMounted(() => {
+  getData()
+})
 </script>
 
 <style scoped></style>
