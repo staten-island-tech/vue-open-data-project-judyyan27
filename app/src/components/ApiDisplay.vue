df4d7e0ee3138e8323f43c6dc40c92c2eefdaba2
Judy <158211720+judyyan27@users.noreply.github.com>
2025-03-06 11:34:15 -0500

    Update ApiDisplay.vue

--- app/src/components/ApiDisplay.vue
+++ app/src/components/ApiDisplay.vue
@@ -1,8 +1,31 @@
 <template>
+  <h2 v-for="(item, index) in fetchedData" :key="index">
+    <span> {{ item.crash_date }} - {{ item.vehicle_type }} </span>
+  </h2>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const fetchedData = ref([])
+
+async function getData() {
+  const res = await fetch('https://data.cityofnewyork.us/resource/bm4k-52h4.json')
+  const data = await res.json()
+  fetchedData.value = data
+  console.log(fetchedData.value)
+}
+
+onMounted(() => {
+  getData()
+})
+</script>
+
+<!-- <template>
   <h2>{{ data }}</h2>
   <ul>
     <li v-for="(data, index) in cart" :key="index">
-      <span> {{ data.crash_date }} - ${{ data.vehicle_typw }} </span>
+      <span> {{ data.crash_date }} - ${{ data.vehicle_type }} </span>
     </li>
   </ul>
 </template>
@@ -26,6 +49,6 @@ async function getData() {
 onMounted(() => {
   getData()
 })
-</script>
+</script> -->
 
 <style scoped></style>
